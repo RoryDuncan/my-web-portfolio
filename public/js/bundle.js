e2dab60e884a6253527c85a0299176ed45248529
Rory Duncan <robust.rory@gmail.com>
2017-09-05 06:26:26 +0000

    Update "about" content

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -1,11 +1,11 @@
 (function () {
 'use strict';
 
-var about = "\n<div class=\"about\">\n  <h1>About Me </h1>\n  <p>I live in Chico, CA where I spend a greater portion of my time working on side projects, or helping friends and colleagues with theirs.</p>\n</div>";
+var about = "\n<div class=\"about\">\n  <h1>About Me </h1>\n  <p>Located in Chico, CA; I spend a greater portion of my time working on side projects, or helping friends with theirs.</p>\n  <p>I have a deep ambition to combine entrepreneurship and software to make society healthier and more educated—and of opening a cat cafe or cat theatre.</p>\n  <h3>Other Interests</h3>\n  <ul class=\"list\">\n    <li>design</li>\n    <li>hiking</li>\n    <li>eSports</li>\n    <li> <a href=\"https://www.youtube.com/watch?v=w6o-QyqP5gQ\">angsty music</a></li>\n    <li>video game development</li>\n    <li>Hayao Miyazaki</li>\n    <li>Wes Anderson</li>\n  </ul>\n</div>";
 
 var introduction = "\n<div class=\"introduction\">\n  <h1>Introduction</h1>\n  <div class=\"skills\">\n    <p id=\"hard-skills\">I have a focused interest in design, user interaction, user experience, developer training and education, process development, authoring content, and code architecture.</p>\n    <p>I intently work by the aphorism <strong>Strong Opinions, Weakly Held</strong>.</p>\n  </div>\n</div>";
 
-var work = "\n<div class=\"work\">\n  <h1>Work and Experience</h1>\n  <p>blah blah blah</p>\n</div>";
+var work = "\n<div class=\"work\">\n  <h1>Work History, Experience, and Projects</h1>\n  <h2>Timeline</h2>\n  <ul class=\"work-history\">\n    <li>\n      <div class=\"business-name\">Work Truck Solutions</div>\n      <div class=\"position\">Software Engineer</div>\n      <div class=\"location\">Chico, CA</div>\n    </li>\n  </ul>\n</div>";
 
 var contact = "\n<div class=\"contact\">\n  <h1>Connecting With Me</h1>\n  <p>blah blah blah</p>\n</div>";
 
