aa7a82d7cd6f80912c94161e3511d336ad7730f3
Rory Duncan <robust.rory@gmail.com>
2017-10-07 09:35:55 +0000

    Add 404 page

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -167,26 +167,30 @@ var renderer = {
 let content = document.querySelectorAll("content")[0];
 let anchors = document.querySelectorAll("nav a");
 
-[].forEach.call( anchors, (el) => {
-  el.addEventListener("click", (e) => {
-    let page = e.target.getAttribute("href"); 
-    e.preventDefault();
-    e.stopPropagation();
-    window.history.pushState(page, null, e.target.getAttribute("href"));
-    changePageContentTo(page);
-    
-    if (page == "/work") {
-      renderer.changeBackgroundEffect("blip-line");
-    }
-    else {
-      renderer.changeBackgroundEffect("blip-grid");
-    }
-    
-    removeActiveNavClass();
-    e.target.classList.add("active");
-  }, false);
-});
+var useHref = document.querySelectorAll(".use-href")[0] || null;
 
+if (!useHref) {
+  
+  [].forEach.call( anchors, (el) => {
+    el.addEventListener("click", (e) => {
+      let page = e.target.getAttribute("href"); 
+      e.preventDefault();
+      e.stopPropagation();
+      window.history.pushState(page, null, e.target.getAttribute("href"));
+      changePageContentTo(page);
+      
+      if (page == "/work") {
+        renderer.changeBackgroundEffect("blip-line");
+      }
+      else {
+        renderer.changeBackgroundEffect("blip-grid");
+      }
+      
+      removeActiveNavClass();
+      e.target.classList.add("active");
+    }, false);
+  });
+}
 const removeActiveNavClass = () => {
   [].forEach.call( anchors, (el) => el.classList.remove("active"));
 };
