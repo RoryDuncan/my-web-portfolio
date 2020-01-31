4577a3f7207f9a1c7fdb9ef3c1f194ab15661a8a
Rory Duncan <robust.rory@gmail.com>
2020-01-30 23:53:09 -0800

    Remove guy

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -38,9 +38,4 @@
 
 <h1>Great success!</h1>
 
-<figure>
-	<img alt='Borat' src='great-success.png'>
-	<figcaption>HIGH FIVE!</figcaption>
-</figure>
-
 <p><strong>Try editing this file (src/routes/index.svelte) to test live reloading.</strong></p>
