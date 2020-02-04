73a23285d4f2dcf7d112e327db89e3f71510acd6
Rory Duncan <robust.rory@gmail.com>
2020-02-04 00:30:35 -0800

    Add title

--- src/routes/links.svelte
+++ src/routes/links.svelte
@@ -2,6 +2,10 @@
   import SocialLinks from "../components/SocialLinks.svelte";
 </script>
 
+<svelte:head>
+	<title>links.meow.coffee</title>
+</svelte:head>
+
 <h1>Links</h1>
 <p>Feel free to reach out to me.</p>
 <SocialLinks showFullLink={true} />
