0d74625eb42029c3d4e0176710c70fc853788750
Rory Duncan <robust.rory@gmail.com>
2020-02-04 18:30:09 -0800

    Update content, link styles

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -1,23 +1,67 @@
 <svelte:head>
-	<title>about.meow.coffee</title>
+  <title>about.meow.coffee</title>
 </svelte:head>
 
 <h1>About Me</h1>
 
-<p>I spend a greater portion of my time working on side projects or helping friends with theirs. I tend to continuously have a side project underway, and I love to collaborate on ideas and projects.</p>
+<p>
+  I spend a greater portion of my time working on side projects or helping
+  friends with theirs. I tend to continuously have a side project underway, and
+  I love to collaborate on ideas.
+</p>
 
 <h2>Interests</h2>
 
-<p>
-  I'm interested in third-wave coffee. When I travel I like to explore local coffee shops and roasteries. I am working to perfect my home pour-over.
-</p>
+<ul>
+  <li>
+    <p>
+      I'm interested in third-wave coffee. When I travel I like to explore local
+      coffee shops and roasteries. I'm honing in on a great home pour-over.
+    </p>
+  </li>
+  <li>
+    <p>
+      Video games and eSports. I especially enjoy
+      <a
+        href="https://apps.quanticfoundry.com/profiles/gamerprofile/xHL3YLUvF6jVKK5GAB35mB/"
+        target="_blank"
+        rel="noopener">
+        couch co-op games and games that are a social experience.
+      </a>
+    </p>
+  </li>
+  <li>
+    <p>
+      Game development, game design, and interactive art. I plan to make my own
+      games soon.
+    </p>
+  </li>
+  <li>
+    <p>
+      I recently starting learning more about cinematography―out of a pursuit of
+      better films and anime. I've been a fan of Hayao Miyazaki and Wes Anderson
+      films for a long time, but I'm on a journey to learn more.
+    </p>
+  </li>
+  <li>
+    <p>
+      I spend way too much time actively looking for new music. I follow the post-punk and post-hardcore genres most actively, but I enjoy many others.
+    </p>
+  </li>
+</ul>
 
-<p>
-  Video games and eSports. I especially enjoy <a href="https://apps.quanticfoundry.com/profiles/gamerprofile/xHL3YLUvF6jVKK5GAB35mB/" target="_blank" rel="noopener">couch co-op games and games that are a social experience</a>.
-</p>
 
-<p>Game development, game design, and interactive art. I plan to make my own games soon.</p>
+<style>
+  a {
+    color: var(--primary-color);
+    text-decoration: none;
+  }
 
-<p>I recently starting learning more about cinematography―out of a pursuit of better films and anime. I'm a fan of Hayao Miyazaki and Wes Anderson films, but I'm on a journey to learn more about the medium.</p>
+  a:hover {
+    text-decoration: underline;
+  }
 
-<p>I spend way too much time actively looking for new music. I stay most up-to-date with the post-punk and post-hardcore genres, but also enjoy diving into other musical expressionism.</p>
+  ul {
+    padding-left: 1em;
+  }
+</style>
